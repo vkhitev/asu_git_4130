6bc26dc365bc9e35e3d67e89ff99b7c4299bf4ff
Vlad Khitev <vkhitev@gmail.com>
2017-03-27 03:00:20 +0300

    Changed shape of asteroid to 8

--- game.js
+++ game.js
@@ -642,16 +642,14 @@ AlienBullet.prototype = new Bullet();
 
 Asteroid = function () {
   this.init("asteroid",
-      [-10,   0,
-        -5,   7,
-        -3,   4,
-        1,  10,
-        5,   4,
-        10,   0,
-        5,  -6,
-        2, -10,
-        -4, -10,
-        -4,  -5]);
+            [-10,   0,
+              -5,   7,
+              -3,   4,
+               1,  10,
+               5,   4,
+              10,   0,
+               5,  -6,
+               2, -10]);
   this.visible = true;
   this.scale = 6;
   this.postMove = this.wrapPostMove;
